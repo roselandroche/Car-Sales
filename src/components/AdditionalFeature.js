a83f465efeae30ff1fe7a2394db92567f83138dd
roselandroche <r.landroche@gmail.com>
2019-10-22 19:30:18 -0400

    Connected AdditionalFeature

--- src/components/AdditionalFeature.js
+++ src/components/AdditionalFeature.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 const AdditionalFeature = props => {
   return (
     <li>
       {/* Add an onClick that will let you add a feature to your car */}
       <button className="button">Add</button>
-      {props.feature.name} (+{props.feature.price})
+      {props.name} (+{props.price})
     </li>
   );
 };
 
-export default AdditionalFeature;
+function mapStateToProps(state) {
+  return {
+    name: state.additionalFeatures.name,
+    price: state.additionalFeatures.price
+  }
+}
+
+export default connect(
+  mapStateToProps
+)(AdditionalFeature);
