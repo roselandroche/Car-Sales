ca59033347548f7e9e4958673719c209b89bcf5f
roselandroche <r.landroche@gmail.com>
2019-10-22 19:03:46 -0400

    Connected Total

--- src/components/Total.js
+++ src/components/Total.js
@@ -3,9 +3,16 @@ import React from 'react';
 const Total = props => {
   return (
     <div className="content">
-      <h4>Total Amount: ${props.car.price + props.additionalPrice}</h4>
+      <h4>Total Amount: ${props.price + props.additionalPrice}</h4>
     </div>
   );
 };
+
+function mapStateToProps(state) {
+  return {
+    price: state.car.price,
+    additionalPrice: state.additionalPrice
+  }
+}
 
 export default Total;
